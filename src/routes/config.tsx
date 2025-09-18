29b4c0b6aaf6d79a9b01872f70a6e8e953dba77a
toohuynh <tu.huynh@4digit.jp>
2025-09-18 17:37:48 +0700

    update logic

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,19 +1,24 @@
-import { lazy } from "react";
+import { lazy, LazyExoticComponent } from "react";
+
+interface IRoute {
+  path: string;
+  element: LazyExoticComponent<React.ComponentType<any>>;
+  children?: IRoute[];
+  index?: boolean;
+}
 
 const CommonLayout = lazy(() => import("~/layouts/common"));
 const Marketplace = lazy(() => import("~/pages/marketplace"));
 
-const RoutesConfig: any[] = [
+const RoutesConfig: IRoute[] = [
   {
     path: "/",
     element: CommonLayout,
     children: [
-      // This makes Marketplace the default component to show at the "/" path.
       {
-        index: true,
+        path: "marketplace",
         element: Marketplace,
       },
-      // The old "/marketplace" path is removed, so it's no longer a valid URL.
     ],
   },
 ];
